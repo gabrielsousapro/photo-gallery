d00654dc6ac5cec89327271a0665440e460b0633
gabrielsousapro <sousadev.pro@gmail.com>
2022-10-31 09:32:22 -0300

    change folder's name

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Open / Close lightbox menu function
+
+function btnOpen() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function btnClose() {
+    document.getElementById("mySidebar").style.display = "none";
+}
